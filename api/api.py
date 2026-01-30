1c79feb478ead8250ea37d0953baa64566f4efd8
NetAcad DEVASC <changeme@example.com>
2026-01-30 16:36:03 +0000

    made application take and return json payload

--- api/api.py
+++ api/api.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 api = Flask(__name__) # creates flask instance
 
 @api.route("/calculator", methods=["POST"]) # defines "/calculator" endpoint for only POST requests
 def api_calculator():
-    return "working\n",200
+    payload = request.get_json(silent=True) # extracts json payload, returns none if it fails
+    return jsonify(payload), 200
 
 if __name__ == "__main__":
     api.run(port=5050) # locally runs on port 5050
