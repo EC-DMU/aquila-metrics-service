73dd562678acd44258da004d4a602a8f2cf867ee
NetAcad DEVASC <changeme@example.com>
2026-01-30 17:51:07 +0000

    added calculation feature working off hard coded input list to test the feature

--- api/api.py
+++ api/api.py
@@ -1,11 +1,21 @@
 from flask import Flask, jsonify, request
+import statistics
 
 api = Flask(__name__) # creates flask instance
 
 @api.route("/calculator", methods=["POST"]) # defines "/calculator" endpoint for only POST requests
 def api_calculator():
-    payload = request.get_json(silent=True) # extracts json payload, returns none if it fails
-    return jsonify(payload), 200
+    # payload = request.get_json(silent=True) # extracts json payload, returns none if it fails
+    data = [19, 24, 55, 31, 16]
+
+    calculations = { # stores results from the calculations below
+        "Mean": statistics.mean(data),
+        "Median": statistics.median(data),
+        "Standard Deviation": statistics.stdev(data),
+        "min": min(data),
+        "Max": max(data)
+    }
+    return jsonify(calculations), 200
 
 if __name__ == "__main__":
     api.run(port=5050) # locally runs on port 5050
