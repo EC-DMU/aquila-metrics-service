fbaaa67388d1f42582753b90f63d4fd4995dac42
Eddie-Chandler <p2798889@my365.dmu.ac.uk>
2026-01-31 00:15:37 +0000

    Fixed spelling errors

--- api/api.py
+++ api/api.py
@@ -8,10 +8,10 @@ def api_calculator():
     payload = request.get_json(silent=True) # extracts json payload, returns none if it fails
     if not payload: # checks if json payload is invalid or missing
         return jsonify({"ERROR": "Invalid Payload"}), 400
-    if "input" not in payload: # checks the payload conatins the "input" key
+    if "input" not in payload: # checks the payload contains the "input" key
         return jsonify({"ERROR": "Missing 'input' Key In Payload"}), 400
     data = payload["input"] # extracts list from the "input" key
-    if len(data) <2: # checks if there are less than two numebrs in the input list
+    if len(data) <2: # checks if there are less than two numbers in the input list
         return jsonify({"ERROR": "Must Contain At Least Two Numbers"}), 400
     if not all(isinstance(x, (int, float))for x in data): # checks the input list contains only numeric inputs
         return jsonify({"ERROR": "Input List Must Contain Only Numbers"}), 400
